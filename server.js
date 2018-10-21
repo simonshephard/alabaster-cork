e2c7f04a7f33209b7efe95fdc89d7b6cdfb682af
Glitch (alabaster-cork) <none>
2018-10-21 08:44:22 +0000

    🚨🎹 Checkpoint ./server.js:1715671/41

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get("/api/test", function (req, res) {
 app.get("/api/timestamp/time", function (req, res) {
 //   take in time as parameter :timestamp
 //   pass to model to get string
-//   
+//   check what type and return correct string
   res.json({
     "unix": 1451001600000, "utc": "Fri, 25 Dec 2015 00:00:00 GMT"
   });
